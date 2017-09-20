e57fed923c5ba7174e870a0086c74628cce43d67
GuiMac <guibasconti@gmail.com>
2017-09-20 03:04:23 -0300

    Added dependencies

---
+++ server/utils/constants.js
@@ -0,0 +1,11 @@
+/**
+ * Módulo de que contém todas as constantes do app
+ * @module utils/constants
+ */
+
+module.exports = {
+  urls: {
+    REDDIT_SUFFIX: 'https://www.reddit.com/r/',
+    REDDIT_PREFFIX: '.json'
+  }
+};
