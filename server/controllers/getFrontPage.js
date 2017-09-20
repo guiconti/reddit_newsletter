ddb91f05178def3a6a8095e096281dea36681166
GuiMac <guibasconti@gmail.com>
2017-09-20 03:19:35 -0300

    Get subreddit front page

---
+++ server/controllers/getFrontPage.js
@@ -0,0 +1,52 @@
+/**
+ * API Module for Reddit front page fetch
+ * @module controllers/reddit
+ */
+
+const underscore = require('underscore');
+const request = require('request');
+const logger = require('../../tools/logger');
+const validation = require('../utils/validation');
+const constants = require('../utils/constants');
+
+/**
+ * Fetch all posts from first page
+ *
+ * @param {string} subreddit - Subreddit name to fetch posts from
+ * @param {express.app.res} res - Posts from subreddit or an error message
+ */
+
+module.exports = (subreddit) => {
+  new Promise((resolve, reject) => {
+    if (!validation.isValidString(subreddit)) return reject({
+      error: constants.messages.error.NOT_VALID_SUBREDDIT
+    });
+  
+    request.get({url: constants.urls.REDDIT_PREFFIX + subreddit + constants.urls.REDDIT_SUFFIX}, function(err, httpResponse, html) {
+      if (err) {
+        return reject({
+          error: constants.messages.error.NON_EXISTENT_SUBREDDIT
+        });
+      }
+      try {
+        let subredditInfo = JSON.parse(html);
+        if (subredditInfo.error) return reject({
+          error: constants.messages.error.NON_EXISTENT_SUBREDDIT
+        });
+        if (subredditInfo.data.children.length == 0){
+          return reject({
+            error: constants.messages.error.NON_EXISTENT_SUBREDDIT
+          });
+        }
+        return resolve({
+          msg: subredditInfo.data.children
+        });
+      } catch (e) {
+        logger.error(e);
+        return reject({
+          error: constants.messages.error.NON_EXISTENT_SUBREDDIT
+        });
+      } 
+    });
+  });
+};
--- server/utils/constants.js
+++ server/utils/constants.js
@@ -7,5 +7,11 @@ module.exports = {
   urls: {
     REDDIT_SUFFIX: 'https://www.reddit.com/r/',
     REDDIT_PREFFIX: '.json'
+  },
+  messages: {
+    error: {
+      NOT_VALID_SUBREDDIT: 'Subreddit informado não é válido.',
+      NON_EXISTENT_SUBREDDIT: 'Subreddit informado não existe.'
+    }
   }
 };
