bd389c27283da58af6d3ee583f243d09f284f690
Guilherme Conti <guibasconti@gmail.com>
2017-10-10 01:48:15 -0300

    Send reply markup on each reddit post

--- server/controllers/sendPosts.js
+++ server/controllers/sendPosts.js
@@ -6,6 +6,20 @@
 const logger = require('../../tools/logger');
 const constants = require('../utils/constants');
 const sendTelegramMessage = require('./sendTelegramMessage');
+const likeButton = JSON.stringify({
+  type: 'reddit', 
+  value: 1
+});
+const dislikeButton = JSON.stringify({
+  type: 'reddit',
+  value: -1
+});
+const inline_keyboard = [
+  [{ text: '\u{1F44D}' , callback_data: likeButton }, { text: '\u{1F44E}' , callback_data: dislikeButton }]
+];
+const options = {
+  reply_markup: {inline_keyboard}
+};
 
 /**
  * Send all posts to a telegram chat
@@ -29,7 +43,8 @@ module.exports = (subreddit, chatId, unformattedPosts) => {
   
     requestInfo.message = constants.messages.info.NEW_POSTS + subreddit + '\n\n';
     sendTelegramMessage(constants.urls.GIBOT, requestInfo);
-  
+    
+    requestInfo.options = options;
     try{
       unformattedPosts.forEach((post) => {
         requestInfo.message = post.title + '\n' + post.url + '\n\n';
